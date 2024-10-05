ecedcc45d02b968ee7b13e5fd0f558f25acdb2b3
alexcao <ahbaby1987@gmail.com>
2024-10-05 07:00:37 +0000

    rollback swap

--- diy-part2_r2c.sh
+++ diy-part2_r2c.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Swap wan and lan port on R2C router
-sed -i "s/'eth1' 'eth0'/'eth0' 'eth1'/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
+#sed -i "s/'eth1' 'eth0'/'eth0' 'eth1'/g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
