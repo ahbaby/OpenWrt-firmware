c2c4920812bed00ee5f1a1792d8027b9e5e85364
Levi.Cao <ahbaby1987@gmail.com>
2025-03-13 14:45:10 +0800

    Replace golang packages

---
+++ diy-custom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
